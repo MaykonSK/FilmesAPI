11ddcccde230fc60c64890f88f27a4cbec10cf80
Maycon Oliveira <Maycon.Oliveira@altagenetics.com>
2022-07-07 11:45:25 -0300

    Aula 7 - Conectando ao banco de dados

---
+++ FilmesAPI/Data/DBContext.cs
@@ -0,0 +1,21 @@
+﻿using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Data
+{
+    public class DBContext : DbContext //essa classe herda da classe DbContext
+    {
+        
+        public DBContext(DbContextOptions<DBContext> option) : base(option) //Construtor
+        {
+
+        }
+
+        //mapear classes
+        public DbSet<Filme> Filmes { get; set; } //DbSet<Filme> é a classe que vamos mapear // Filmes é o nome da tabela banco de dados
+    }
+}
--- FilmesAPI/Startup.cs
+++ FilmesAPI/Startup.cs
@@ -1,8 +1,10 @@
+using FilmesAPI.Data;
 using FilmesAPI.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,8 @@ namespace FilmesAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //configura o banco de dados // DBContext é o nome da classe de conexao criada
+            services.AddDbContext<DBContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("FilmesConexao")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
